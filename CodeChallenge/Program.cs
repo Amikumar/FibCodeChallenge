7db7ba46a9a37589d3be5f38861bf303c4321582
Amit Kumar <netamitk@gmail.com>
2019-04-28 16:34:57 +0530

    adding , to output

--- CodeChallenge/Program.cs
+++ CodeChallenge/Program.cs
@@ -13,15 +13,15 @@ namespace FibSequenceGenerator
 			var inputSeqArray = new List<int> { 1,1 };
 			var n = 6;
 			var result= FibSequence.GenerateFibSequence(inputSeqArray, n);
-			Console.WriteLine("Output for input sequence { 0, 1, 1 } n=6 ={" + string.Join(" ", result) + " }");
+			Console.WriteLine("Output for input sequence { 0, 1, 1 } n=6 ={" + string.Join(",", result) + " }");
 			inputSeqArray = new List<int> { 0, 1, 1 };
 			 n = 10;
 			result = FibSequence.GenerateFibSequence(inputSeqArray, n);
-			Console.WriteLine("Output for input sequence { 0, 1, 1 } n=10 ={"+ string.Join(" ", result)+" }");
+			Console.WriteLine("Output for input sequence { 0, 1, 1 } n=10 ={"+ string.Join(",", result)+" }");
 			inputSeqArray = new List<int> { 2, 4, 6, 8 };
 			n = 12;
 			result = FibSequence.GenerateFibSequence(inputSeqArray, n);
-			Console.WriteLine("Output for input sequence {2, 4, 6, 8  } n=12 ={" + string.Join(" ", result) + " }");
+			Console.WriteLine("Output for input sequence {2, 4, 6, 8  } n=12 ={" + string.Join(",", result) + " }");
 			Console.Read();
 		}
 		
